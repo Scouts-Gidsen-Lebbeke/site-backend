317d9b70f80a7c7da4c9de17c53f368fa9241b59
rokeppen <robinkep@gmail.com>
2024-11-17 00:30:51 +0100

    Fix the UserMapper

--- src/main/kotlin/be/sgl/backend/service/user/UserService.kt
+++ src/main/kotlin/be/sgl/backend/service/user/UserService.kt
@@ -1,6 +1,7 @@
 package be.sgl.backend.service.user
 
 import be.sgl.backend.dto.UserDTO
+import be.sgl.backend.service.mapper.UserMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Service
 class UserService {
 
     @Autowired
+    private lateinit var mapper: UserMapper
+    @Autowired
     private lateinit var userDataProvider: UserDataProvider
 
     fun getProfile(username: String): UserDTO {
-        return UserDTO(user, userDataProvider.getUserData(user))
+        return mapper.toDto(userDataProvider.getUser(username))
     }
 }
