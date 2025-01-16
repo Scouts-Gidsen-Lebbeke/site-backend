5af8ddae34291633b4b91413a70b5d89d55dd6bb
rokeppen <robinkep@gmail.com>
2025-01-16 23:05:01 +0100

    Add id and createdDate fields

--- src/main/kotlin/be/sgl/backend/dto/NewsItemDTO.kt
+++ src/main/kotlin/be/sgl/backend/dto/NewsItemDTO.kt
@@ -2,13 +2,16 @@ package be.sgl.backend.dto
 
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
+import java.time.LocalDateTime
 
 data class NewsItemDTO(
+    var id: Int? = null,
     @NotBlank(message = "{NotBlank.newsItem.title}")
     @Size(max = 50, message = "{Size.newsItem.title}")
     var title: String,
     @NotBlank(message = "{NotBlank.newsItem.content}")
     @Size(max = 500, message = "{Size.newsItem.content}")
     var content: String,
-    var image: String? = null
+    var image: String? = null,
+    var createdDate: LocalDateTime?
 )
