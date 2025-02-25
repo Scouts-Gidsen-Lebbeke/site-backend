4c4a3673925b6b2cb309ff19663242df227eb864
rokeppen <robinkep@gmail.com>
2025-02-25 23:47:41 +0100

    Cleaner branch endpoints

--- src/main/kotlin/be/sgl/backend/repository/BranchRepository.kt
+++ src/main/kotlin/be/sgl/backend/repository/BranchRepository.kt
@@ -1,7 +1,6 @@
 package be.sgl.backend.repository
 
 import be.sgl.backend.entity.branch.Branch
-import be.sgl.backend.entity.branch.BranchStatus
 import be.sgl.backend.entity.user.Sex
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -9,7 +8,10 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface BranchRepository : JpaRepository<Branch, Int> {
-    fun getByStatusIn(statuses: Collection<BranchStatus>): List<Branch>
+    @Query("from Branch where status = 'ACTIVE'")
+    fun getBranchesWithCalendar(): List<Branch>
+    @Query("from Branch where status != 'HIDDEN'")
+    fun getVisibleBranches(): List<Branch>
     @Query("from Branch where :age between minimumAge and maximumAge and (sex is null or sex = :sex) and status != 'HIDDEN' order by status")
     fun getPossibleBranchesForSexAndAge(sex: Sex, age: Int): List<Branch>
 }
