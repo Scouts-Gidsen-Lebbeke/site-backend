c5600a9c37f69d06f73364eaef62953318dec8ad
rokeppen <robinkep@gmail.com>
2025-03-11 20:35:13 +0100

    Ffs

--- src/main/kotlin/be/sgl/backend/entity/organization/Organization.kt
+++ src/main/kotlin/be/sgl/backend/entity/organization/Organization.kt
@@ -22,8 +22,7 @@ class Organization : Auditable() {
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "organization", cascade = [(CascadeType.ALL)])
     val contactMethods: MutableList<ContactMethod> = mutableListOf()
     var image: String? = null
-    @Lob
-    @Column(columnDefinition = "TEXT")
+    @Column(nullable = false, length = 1000)
     var description: String? = null
 
     fun getEmail(): String? {
