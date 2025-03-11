4d77f4da9efaec8c3e3e1318bb88c769c83dc3b7
Manfred Lambrecht <Manfred@lama.be>
2025-03-11 21:00:29 +0100

    Revert "add contextpath to webconfig"
    This reverts commit e073f7b5f3943c87d34420ae8d27b2e98275ada6.

--- src/main/kotlin/be/sgl/backend/config/WebConfig.kt
+++ src/main/kotlin/be/sgl/backend/config/WebConfig.kt
@@ -1,7 +1,6 @@
 package be.sgl.backend.config
 
 import be.sgl.backend.service.ImageService.Companion.IMAGE_BASE_PATH
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -9,11 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
 
-    @Value("\${server.servlet.context-path:}")
-    private lateinit var contextPath: String
-
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("$contextPath/$IMAGE_BASE_PATH/**")
+        registry.addResourceHandler("/$IMAGE_BASE_PATH/**")
             .addResourceLocations("file:$IMAGE_BASE_PATH/")
     }
 }
