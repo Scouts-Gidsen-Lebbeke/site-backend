871027b7f23e53096590f41dee3bc4d7b1021b8f
rokeppen <robinkep@gmail.com>
2025-03-20 01:33:01 +0100

    Calendar fixes

--- src/main/kotlin/be/sgl/backend/dto/CalendarDTO.kt
+++ src/main/kotlin/be/sgl/backend/dto/CalendarDTO.kt
@@ -1,6 +1,10 @@
 package be.sgl.backend.dto
 
+import be.sgl.backend.util.StartEndDate
+import be.sgl.backend.util.StartEndTime
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -23,18 +27,20 @@ class CalendarDTO(
 
 // for item listing
 open class CalendarItemDTO(
-    val id: Int?,
-    val start: LocalDateTime,
-    val end: LocalDateTime,
-    @NotBlank(message = "{NotBlank.calendarItem.title}")
-    @Size(max = 50, message = "{Size.calendarItem.title}")
-    val title: String,
-    @NotBlank(message = "{NotBlank.calendarItem.content}")
-    val content: String,
-    val image: String?,
-    val closed: Boolean,
-    val address: AddressDTO?
-)
+    var id: Int?,
+    @field:NotNull(message = "{NotNull.calendarItem.start}")
+    override var start: LocalDateTime,
+    @field:NotNull(message = "{NotNull.calendarItem.end}")
+    override var end: LocalDateTime,
+    @field:NotBlank(message = "{NotBlank.calendarItem.title}")
+    @field:Size(max = 50, message = "{Size.calendarItem.title}")
+    var title: String,
+    @field:NotBlank(message = "{NotBlank.calendarItem.content}")
+    var content: String,
+    var image: String?,
+    var closed: Boolean,
+    var address: AddressDTO?
+) : StartEndTime
 
 // for item edits
 class CalendarItemWithCalendarsDTO(
@@ -46,14 +52,17 @@ class CalendarItemWithCalendarsDTO(
     image: String?,
     closed: Boolean,
     address: AddressDTO?,
-    val calendars: MutableList<CalendarBaseDTO>
+    @field:NotEmpty(message = "{NotEmpty.calendarItem.calendars}")
+    var calendars: MutableList<CalendarBaseDTO>
 ) : CalendarItemDTO(id, start, end, title, content, image, closed, address)
 
 data class CalendarPeriodDTO(
-    val id: Int?,
-    @NotBlank(message = "{NotBlank.calendarPeriod.name}")
-    @Size(max = 50, message = "{Size.calendarPeriod.name}")
-    val name: String,
-    val start: LocalDate,
-    val end: LocalDate
-)
+    var id: Int?,
+    @field:NotBlank(message = "{NotBlank.calendarPeriod.name}")
+    @field:Size(max = 50, message = "{Size.calendarPeriod.name}")
+    var name: String,
+    @field:NotNull(message = "{NotNull.calendarPeriod.start}")
+    override var start: LocalDate,
+    @field:NotNull(message = "{NotNull.calendarPeriod.end}")
+    override var end: LocalDate
+) : StartEndDate
