db3c78eb35c6cf773e84c62991cd1995f947c565
rokeppen <robinkep@gmail.com>
2025-05-01 12:05:17 +0200

    Add apiResponse specs and remove todo

--- src/main/kotlin/be/sgl/backend/config/security/LevelSecurityService.kt
+++ src/main/kotlin/be/sgl/backend/config/security/LevelSecurityService.kt
@@ -2,9 +2,13 @@ package be.sgl.backend.config.security
 
 import be.sgl.backend.entity.user.RoleLevel
 import be.sgl.backend.service.user.UserDataProvider
+import io.github.wimdeblauwe.errorhandlingspringbootstarter.ApiErrorResponse
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
@@ -21,7 +25,6 @@ class LevelSecurityService {
 
     private fun currentUserHasRoleWithLevel(level: RoleLevel): Boolean {
         val authentication = SecurityContextHolder.getContext().authentication
-        // TODO: if a user without a known (but valid) login logs in, we get NPEs all over the place
         val user = userDataProvider.findUser(authentication?.name ?: return false) ?: return false
         return user.level >= level
     }
@@ -31,21 +34,21 @@ class LevelSecurityService {
 @Retention(AnnotationRetention.RUNTIME)
 @SecurityRequirement(name = "bearerAuth")
 @PreAuthorize("@levelSecurityService.isAdmin()")
-@ApiResponse(responseCode = "403", description = "User has no admin role")
+@ApiResponse(responseCode = "403", description = "User has no admin role", content = [Content(mediaType = APPLICATION_JSON_VALUE, schema = Schema(implementation = ApiErrorResponse::class))])
 annotation class OnlyAdmin
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @SecurityRequirement(name = "bearerAuth")
 @PreAuthorize("@levelSecurityService.isStaff()")
-@ApiResponse(responseCode = "403", description = "User has no staff role")
+@ApiResponse(responseCode = "403", description = "User has no staff role", content = [Content(mediaType = APPLICATION_JSON_VALUE, schema = Schema(implementation = ApiErrorResponse::class))])
 annotation class OnlyStaff
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @SecurityRequirement(name = "bearerAuth")
 @PreAuthorize("isAuthenticated()")
-@ApiResponse(responseCode = "403", description = "User is not logged in")
+@ApiResponse(responseCode = "403", description = "User is not logged in", content = [Content(mediaType = APPLICATION_JSON_VALUE, schema = Schema(implementation = ApiErrorResponse::class))])
 annotation class OnlyAuthenticated
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
